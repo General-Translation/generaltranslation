e8526aaf02c74c90c2d8e18fbaea4e41151ff0da
archie-mckenzie <archie@generaltranslation.com>
2024-03-11 15:49:10 -0400

    update

--- models/models.js
+++ models/models.js
@@ -4,21 +4,27 @@
 // Returns an object or null if invalid
 const getModelInfo = async model => {
     model = model?.toLowerCase();
-    const result = await fetch(`https://gtx.dev/model/${model}`);
-    const modelInfo = await result.json();
-    return modelInfo;
+    try {
+        const result = await fetch(`https://gtx.dev/model/${model}`);
+        const modelInfo = await result.json();
+        return modelInfo;
+    } catch {
+        return null;
+    }
 }
 
 // Get all languages known to be compatible with a given LLM
 // Returns an array of languages codes, [] if unknown
 const getModelLanguages = async model => {
-    return (await getModelInfo(model))?.languages || [];
+    const modelInfo = await getModelInfo(model);
+    return modelInfo?.languages || [];
 }
 
 // Returns true if a model is known to be compatible with a language
 // Returns false otherwise
 const isLanguageSupported = async (model, code) => {
-    return (await getModelLanguages(model))?.includes(code);
+    const modelLanguages = await getModelLanguages(model);
+    return modelLanguages?.includes(code);
 }
 
 module.exports = {
