11aefbbe0511462b0cf3cb6150e567988706f215
archie-mckenzie <archie@generaltranslation.com>
2024-05-08 19:23:50 -0400

    multi letter codes

--- codes/codes.js
+++ codes/codes.js
@@ -10,7 +10,17 @@ const LanguageToCodeTriletter = require('./639-3/LanguageToCodeTriletter.json');
 // ----- LANGUAGE CODES ----- //
 
 // Returns the name of a language from an ISO 639 code or an array of codes
-const _mapCodeToLanguage = code => CodeToLanguage[code] || CodeToLanguageTriletter[code] || '';
+const _mapCodeToLanguage = code => {
+    if (code.length === 2) {
+        return CodeToLanguage[code]
+    } 
+    if (code.length === 3) {
+        CodeToLanguageTriletter[code]
+    }
+    else {
+        return CodeToLanguage[code.slice(0, 2)] || '';
+    }
+}
 const _getLanguageName = codes => {
     return Array.isArray(codes) ? codes.map(_mapCodeToLanguage) : _mapCodeToLanguage(codes);
 }
