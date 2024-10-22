373fb18dba8b4b13b8392955eccbb9de0996b08f
archie-mckenzie <archie@generaltranslation.com>
2024-10-21 18:51:13 -0700

    requires translation edge cases

--- src/codes/_requiresTranslation.ts
+++ src/codes/_requiresTranslation.ts
@@ -10,8 +10,21 @@ import _isSameLanguage from "./_isSameLanguage";
 export default function _requiresTranslation(
     sourceLanguage: string, targetLanguage: string, approvedLanguages?: string[]
 ): boolean {
+
+    // if no target
     if (!targetLanguage) return false;
+
+    // if the codes are exactly the same
     if (_standardizeLanguageCode(targetLanguage) === _standardizeLanguageCode(sourceLanguage)) return false;
+
+    // if the codes are more/less specific versions of each other, e.g. en and en-US
+    const { language: sourceLanguageCode, region: sourceRegion } = new Intl.Locale(sourceLanguage);
+    const { language: targetLanguageCode, region: targetRegion } = new Intl.Locale(targetLanguage);
+    if ((!sourceRegion || !targetRegion) && _isSameLanguage(sourceLanguageCode, targetLanguageCode)) return false;
+
+    // if no translation is possible
     if (approvedLanguages && !approvedLanguages.some(approvedLanguage => _isSameLanguage(targetLanguage, approvedLanguage))) return false;
+    
+    // otherwise, a translation is required!
     return true;
 }
