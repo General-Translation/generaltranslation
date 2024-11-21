96468ea6a52d93d8aa3ce983cfc1c9f9cfe6bb61
archie-mckenzie <archie@generaltranslation.com>
2024-11-21 15:58:03 -0800

    exporting localeHeaderName

--- dist/internal.d.ts
+++ dist/internal.d.ts
@@ -3,6 +3,7 @@ declare const localeCookieName: "generaltranslation.locale";
 declare const pluralForms: readonly ["singular", "plural", "dual", "zero", "one", "two", "few", "many", "other"];
 declare const defaultCacheURL: "https://cache.gtx.dev";
 declare const defaultAPIURL: "https://prod.gtx.dev";
+declare const localeHeaderName = "x-generaltranslation-locale";
 declare function isAcceptedPluralForm(form: string): form is (typeof pluralForms)[number];
 
-export { defaultAPIURL, defaultCacheURL, isAcceptedPluralForm, libraryDefaultLocale, localeCookieName, pluralForms };
+export { defaultAPIURL, defaultCacheURL, isAcceptedPluralForm, libraryDefaultLocale, localeCookieName, localeHeaderName, pluralForms };
--- src/internal.ts
+++ src/internal.ts
@@ -3,6 +3,7 @@ export const localeCookieName = "generaltranslation.locale" as const;
 export const pluralForms = ["singular", "plural", "dual", "zero", "one", "two", "few", "many", "other"] as const;
 export const defaultCacheURL = "https://cache.gtx.dev" as const;
 export const defaultAPIURL = "https://prod.gtx.dev" as const;
+export const localeHeaderName = "x-generaltranslation-locale";
 export function isAcceptedPluralForm(
     form: string
   ): form is (typeof pluralForms)[number] {
