6dde2dc9083b265b6717fbe23b63425e07e4cfdc
ErnestM1234 <ernestm@princeton.edu>
2024-12-19 16:52:44 -0800

    add deleted files

--- src/translationProcessing/_isResultError.ts
+++
@@ -1,8 +0,0 @@
-import { ContentTranslationResult, JsxTranslationResult, TranslationError } from "src/types";
-
-/**
- * @internal
- */
-export default function _isResultError(result: JsxTranslationResult | ContentTranslationResult | TranslationError): result is TranslationError {
-    return 'error' in result;
-}
--- src/translationProcessing/_isResultSuccessful.ts
+++
@@ -1,8 +0,0 @@
-import { ContentTranslationResult, JsxTranslationResult, TranslationError } from "src/types";
-
-/**
- * @internal
- */
-export default function _isResultSuccessful(result: JsxTranslationResult | ContentTranslationResult | TranslationError): result is TranslationError {
-    return 'translation' in result;
-}
